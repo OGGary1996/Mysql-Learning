6d1bd5b93cd4e5bcd670e402d3323e96f9ab073b
ke_zhang <zhangke918525@gmail.com>
2024-09-25 20:40:17 -0600

    recreate bookstoredb

--- BookstoreDB/bookstoredb.sql
+++ BookstoreDB/bookstoredb.sql
@@ -1,13 +1,15 @@
--- MySQL dump 10.13  Distrib 8.0.39, for macos14 (arm64)
+CREATE DATABASE  IF NOT EXISTS `bookstoredb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `bookstoredb`;
+-- MySQL dump 10.13  Distrib 8.0.38, for macos14 (arm64)
 --
--- Host: localhost    Database: BookstoreDB
+-- Host: localhost    Database: bookstoredb
 -- ------------------------------------------------------
 -- Server version	8.0.39
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
+/*!50503 SET NAMES utf8 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -16,13 +18,13 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `Books`
+-- Table structure for table `books`
 --
 
-DROP TABLE IF EXISTS `Books`;
+DROP TABLE IF EXISTS `books`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `Books` (
+CREATE TABLE `books` (
   `Book_id` int NOT NULL,
   `Title` varchar(50) DEFAULT NULL,
   `Writers_id` int NOT NULL,
@@ -36,64 +38,49 @@ CREATE TABLE `Books` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `Books`
+-- Dumping data for table `books`
 --
 
-LOCK TABLES `Books` WRITE;
-/*!40000 ALTER TABLE `Books` DISABLE KEYS */;
-INSERT INTO `Books` VALUES (1,'HONGLOUMENG',1,1920,NULL),(2,'SANGUOYANYI',2,1930,NULL),(3,'XIYOUJI',3,1945,NULL),(4,'SHUIHUZHUAN',4,1937,NULL),(5,'ZHAOHUAXISHI',5,1928,NULL);
-/*!40000 ALTER TABLE `Books` ENABLE KEYS */;
+LOCK TABLES `books` WRITE;
+/*!40000 ALTER TABLE `books` DISABLE KEYS */;
+INSERT INTO `books` VALUES (1,'HONGLOUMENG',1,1920,NULL),(2,'SANGUOYANYI',2,1930,NULL),(3,'XIYOUJI',3,1945,NULL),(4,'SHUIHUZHUAN',4,1937,NULL),(5,'ZHAOHUAXISHI',5,1928,NULL);
+/*!40000 ALTER TABLE `books` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `test_table`
+-- Table structure for table `writers`
 --
 
-DROP TABLE IF EXISTS `test_table`;
+DROP TABLE IF EXISTS `writers`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `test_table` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `test_table`
---
-
-LOCK TABLES `test_table` WRITE;
-/*!40000 ALTER TABLE `test_table` DISABLE KEYS */;
-/*!40000 ALTER TABLE `test_table` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `Writers`
---
-
-DROP TABLE IF EXISTS `Writers`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `Writers` (
+CREATE TABLE `writers` (
   `Writers_id` int NOT NULL,
   `First_Name` varchar(20) DEFAULT NULL,
   `Last_Name` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`Writers_id`),
   UNIQUE KEY `Author_id` (`Writers_id`),
-  CONSTRAINT `writers_ibfk_1` FOREIGN KEY (`Writers_id`) REFERENCES `Books` (`Writers_id`)
+  CONSTRAINT `writers_ibfk_1` FOREIGN KEY (`Writers_id`) REFERENCES `books` (`Writers_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `Writers`
+-- Dumping data for table `writers`
 --
 
-LOCK TABLES `Writers` WRITE;
-/*!40000 ALTER TABLE `Writers` DISABLE KEYS */;
-INSERT INTO `Writers` VALUES (1,'Xue_qin','Cao'),(2,'Guan_zhong','Luo'),(3,'Cheng_en','Wu'),(4,'Nai_an','Shi'),(5,'Xun','Lu');
-/*!40000 ALTER TABLE `Writers` ENABLE KEYS */;
+LOCK TABLES `writers` WRITE;
+/*!40000 ALTER TABLE `writers` DISABLE KEYS */;
+INSERT INTO `writers` VALUES (1,'Xue_qin','Cao'),(2,'Guan_zhong','Luo'),(3,'Cheng_en','Wu'),(4,'Nai_an','Shi'),(5,'Xun','Lu');
+/*!40000 ALTER TABLE `writers` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Dumping events for database 'bookstoredb'
+--
+
+--
+-- Dumping routines for database 'bookstoredb'
+--
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -104,4 +91,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-09-23 22:16:55
+-- Dump completed on 2024-09-25 20:38:33
